4042cdb26c89c05d09ce976816fa9b467af56782
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-02-04 10:08:16 -0500

    added roxygen comments and .R file extension

--- R/basic-types.R
+++ R/basic-types.R
@@ -1,33 +1,54 @@
+#' Mission Design Series Basic Type
+#' 
+#' Abc.
+#'
+#' @param x A character vector.
+#' @param ... Additional optional arguments to be passed onto \code{grepl}.
+#'
+#' @rdname basic-type
+#' @export
 is_ground_attack <- function(x, ...) {
   grepl("A[DGHQSVZ]?-?[0-9]{1,3}[ABCDEFGHJKLMNPQRSTUVWXYZ]*", x, ...)
 }
 
 
+#' @rdname basic-type
+#' @export
 is_bomber <- function(x, ...) {
   grepl("B[DGHQSVZ]?-?[0-9]{1,3}[ABCDEFGHJKLMNPQRSTUVWXYZ]*", x, ...)
 }
 
 
+#' @rdname basic-type
+#' @export
 is_transport <- function(x, ...) {
   grepl("C[DGHQSVZ]?-?[0-9]{1,3}[ABCDEFGHJKLMNPQRSTUVWXYZ]*", x, ...)
 }
 
 
+#' @rdname basic-type
+#' @export
 is_fighter <- function(x, ...) {
   grepl("F[DGHQSVZ]?-?[0-9]{1,3}[ABCDEFGHJKLMNPQRSTUVWXYZ]*", x, ...)
 }
 
 
+#' @rdname basic-type
+#' @export
 is_tanker <- function(x, ...) {
   grepl("K[DGHQSVZ]?-?[0-9]{1,3}[ABCDEFGHJKLMNPQRSTUVWXYZ]*", x, ...)
 }
 
 
+#' @rdname basic-type
+#' @export
 is_multi_mission <- function(x, ...) {
   grepl("M[DGHQSVZ]?-?[0-9]{1,3}[ABCDEFGHJKLMNPQRSTUVWXYZ]*", x, ...)
 }
 
 
+#' @rdname basic-type
+#' @export
 is_trainer <- function(x, ...) {
   grepl("T[DGHQSVZ]?-?[0-9]{1,3}[ABCDEFGHJKLMNPQRSTUVWXYZ]*", x, ...)
 }
